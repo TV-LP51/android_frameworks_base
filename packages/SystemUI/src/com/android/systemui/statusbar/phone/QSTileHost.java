3b3a9b888b418fd778a04665d8950f21c53b2a7f
eyosen <abittin@gmail.com>
2015-02-05 13:49:09 +0200

    QS: add Screenrecord tile (1/2)
    Change-Id: I2df2ff0c8b81eb77b3d112d357cdf18cb19f4c2b
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -63,6 +63,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.VisualizerTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
+import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.AppCircleBarTile;
@@ -337,6 +338,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ScreenshotTile(this);
             case QSConstants.TILE_HEADS_UP:
                 return new HeadsUpTile(this);
+            case QSConstants.TILE_SCREENRECORD:
+                return new ScreenrecordTile(this);
             case QSConstants.TILE_SYNC:
                 return new SyncTile(this);
             case QSConstants.TILE_AMBIENT_DISPLAY:

sooorajjj/frameworks_base

Base: Gesture Anywhere [1/2]
Change-Id: I04acb9a88fe0011921a1bafdc1919d76ce57aedf

Conflicts:
	core/java/android/provider/Settings.java

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -19,6 +19,9 @@ package com.android.systemui.statusbar;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.TimeInterpolator;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.Notification;
@@ -42,6 +45,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.ThemeConfig;
 import android.database.ContentObserver;
+import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -66,6 +70,7 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.util.SparseBooleanArray;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.IWindowManager;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -99,6 +104,7 @@ import com.android.systemui.SearchPanelView;
 import com.android.systemui.SwipeHelper;
 import com.android.systemui.SystemUI;
 import com.android.systemui.cm.SpamMessageProvider;
+import com.android.systemui.sokp.gestureanywhere.GestureAnywhereView;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.phone.NavigationBarView;
@@ -242,6 +248,9 @@ public abstract class BaseStatusBar extends SystemUI implements
     private RecentsComponent mRecents;
 
     protected int mZenMode;
+
+    @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_FIELD)
+    protected GestureAnywhereView mGestureAnywhereView;
 
     // which notification is currently being longpress-examined by the user
     private NotificationGuts mNotificationGutsExposed;
@@ -2212,5 +2221,39 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     public boolean isKeyguardSecure() {
         return mStatusBarKeyguardViewManager.isSecure();
+    }
+
+    @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_METHOD)
+    protected void addGestureAnywhereView() {
+        mGestureAnywhereView = (GestureAnywhereView)View.inflate(
+                mContext, R.layout.gesture_anywhere_overlay, null);
+        mWindowManager.addView(mGestureAnywhereView, getGestureAnywhereViewLayoutParams(Gravity.LEFT));
+        mGestureAnywhereView.setStatusBar(this);
+    }
+
+    @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_METHOD)
+    protected void removeGestureAnywhereView() {
+        if (mGestureAnywhereView != null)
+            mWindowManager.removeView(mGestureAnywhereView);
+    }
+
+    @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_METHOD)
+    protected WindowManager.LayoutParams getGestureAnywhereViewLayoutParams(int gravity) {
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_STATUS_BAR_SUB_PANEL,
+                0
+                | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
+                PixelFormat.TRANSLUCENT);
+        lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_NO_MOVE_ANIMATION;
+        lp.gravity = Gravity.TOP | gravity;
+        lp.setTitle("GestureAnywhereView");
+
+        return lp;
     }
 }

packages/SystemUI/src/com/android/systemui/sokp/TriggerOverlayView.java
@@ -1,0 +1,144 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.sokp;
+
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import com.android.systemui.R;
+
+public abstract class TriggerOverlayView extends FrameLayout {
+    final protected WindowManager mWM;
+
+    protected WindowManager.LayoutParams mLayoutParams;
+    protected int mTriggerWidth;
+    protected int mTriggerTop;
+    protected int mTriggerBottom;
+    protected int mViewHeight;
+
+    public TriggerOverlayView(Context context) {
+        this(context, null);
+    }
+
+    public TriggerOverlayView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TriggerOverlayView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mWM = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        mViewHeight = getWindowHeight();
+    }
+
+    protected int getWindowHeight() {
+        Rect r = new Rect();
+        getWindowVisibleDisplayFrame(r);
+        return r.bottom - r.top;
+    }
+
+    protected int enableKeyEvents() {
+        return (0
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH);
+    }
+
+    protected int disableKeyEvents() {
+        return (0
+                | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH);
+    }
+
+    protected void setPosition(int gravity) {
+        mLayoutParams = (WindowManager.LayoutParams) getLayoutParams();
+        mLayoutParams.gravity = Gravity.TOP | gravity;
+        mWM.updateViewLayout(this, mLayoutParams);
+        invalidate();
+    }
+
+    protected void showTriggerRegion() {
+        setBackgroundResource(R.drawable.trigger_region);
+    }
+
+    protected void hideTriggerRegion() {
+        setBackgroundColor(0);
+    }
+
+    protected void setTopPercentage(float value) {
+        mLayoutParams = (WindowManager.LayoutParams)this.getLayoutParams();
+        mTriggerTop = (int)(mViewHeight * value);
+        mLayoutParams.y = mTriggerTop;
+        mLayoutParams.height = mTriggerBottom;
+        try {
+            mWM.updateViewLayout(this, mLayoutParams);
+        } catch (Exception e) {
+        }
+    }
+
+    protected void setBottomPercentage(float value) {
+        mLayoutParams = (WindowManager.LayoutParams)this.getLayoutParams();
+        mTriggerBottom = (int)(mViewHeight * value);
+        mLayoutParams.height = mTriggerBottom;
+        try {
+            mWM.updateViewLayout(this, mLayoutParams);
+        } catch (Exception e) {
+        }
+    }
+
+    protected void setTriggerWidth(int value) {
+        mLayoutParams = (WindowManager.LayoutParams)this.getLayoutParams();
+        mTriggerWidth = value;
+        mLayoutParams.width = mTriggerWidth;
+        try {
+            mWM.updateViewLayout(this, mLayoutParams);
+        } catch (Exception e) {
+        }
+    }
+
+    protected void expandFromTriggerRegion() {
+        mLayoutParams = (WindowManager.LayoutParams) getLayoutParams();
+        mLayoutParams.y = 0;
+        Rect r = new Rect();
+        getWindowVisibleDisplayFrame(r);
+        mViewHeight = r.bottom - r.top;
+        mLayoutParams.height = mViewHeight;
+        mLayoutParams.width = LayoutParams.MATCH_PARENT;
+        mLayoutParams.flags = enableKeyEvents();
+        mWM.updateViewLayout(this, mLayoutParams);
+    }
+
+    protected void reduceToTriggerRegion() {
+        mLayoutParams = (WindowManager.LayoutParams) getLayoutParams();
+        mLayoutParams.y = mTriggerTop;
+        mLayoutParams.height = mTriggerBottom;
+        mLayoutParams.width = mTriggerWidth;
+        mLayoutParams.flags = disableKeyEvents();
+        mWM.updateViewLayout(this, mLayoutParams);
+    }
+
+    protected boolean isKeyguardEnabled() {
+        KeyguardManager km = (KeyguardManager)mContext.getSystemService(Context.KEYGUARD_SERVICE);
+        return km.inKeyguardRestrictedInputMode();
+    }
+}

TV-LP51/android_frameworks_base

Signed-off-by: martinusbe <martinusbe@gmail.com>
	packages/SystemUI/res/drawable/trigger_region.xml
	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -19,6 +19,9 @@ package com.android.systemui.statusbar;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.TimeInterpolator;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.Notification;
@@ -42,6 +45,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.ThemeConfig;
 import android.database.ContentObserver;
+import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -104,6 +108,7 @@ import com.android.systemui.SearchPanelView;
 import com.android.systemui.SwipeHelper;
 import com.android.systemui.SystemUI;
 import com.android.systemui.cm.SpamMessageProvider;
+import com.android.systemui.validus.gestureanywhere.GestureAnywhereView;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.appcirclesidebar.AppCircleSidebar;
@@ -250,6 +255,9 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected int mZenMode;
 
     protected AppCircleSidebar mAppCircleSidebar;
+
+    @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_FIELD)
+    protected GestureAnywhereView mGestureAnywhereView;
 
     // which notification is currently being longpress-examined by the user
     private NotificationGuts mNotificationGutsExposed;
@@ -2285,6 +2293,40 @@ public abstract class BaseStatusBar extends SystemUI implements
         return mStatusBarKeyguardViewManager.isSecure();
     }
 
+    @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_METHOD)
+    protected void addGestureAnywhereView() {
+        mGestureAnywhereView = (GestureAnywhereView)View.inflate(
+                mContext, R.layout.gesture_anywhere_overlay, null);
+        mWindowManager.addView(mGestureAnywhereView, getGestureAnywhereViewLayoutParams(Gravity.LEFT));
+        mGestureAnywhereView.setStatusBar(this);
+    }
+
+    @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_METHOD)
+    protected void removeGestureAnywhereView() {
+        if (mGestureAnywhereView != null)
+            mWindowManager.removeView(mGestureAnywhereView);
+    }
+
+    @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_METHOD)
+    protected WindowManager.LayoutParams getGestureAnywhereViewLayoutParams(int gravity) {
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_STATUS_BAR_SUB_PANEL,
+                0
+                | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
+                PixelFormat.TRANSLUCENT);
+        lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_NO_MOVE_ANIMATION;
+        lp.gravity = Gravity.TOP | gravity;
+        lp.setTitle("GestureAnywhereView");
+
+        return lp;
+    }
+        
     protected void addAppCircleSidebar() {
         if (mAppCircleSidebar == null) {
             mAppCircleSidebar = (AppCircleSidebar) View.inflate(mContext, R.layout.app_circle_sidebar, null);

packages/SystemUI/src/com/android/systemui/validus/TriggerOverlayView.java
@@ -1,0 +1,144 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.validus;
+
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import com.android.systemui.R;
+
+public abstract class TriggerOverlayView extends FrameLayout {
+    final protected WindowManager mWM;
+
+    protected WindowManager.LayoutParams mLayoutParams;
+    protected int mTriggerWidth;
+    protected int mTriggerTop;
+    protected int mTriggerBottom;
+    protected int mViewHeight;
+
+    public TriggerOverlayView(Context context) {
+        this(context, null);
+    }
+
+    public TriggerOverlayView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TriggerOverlayView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mWM = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        mViewHeight = getWindowHeight();
+    }
+
+    protected int getWindowHeight() {
+        Rect r = new Rect();
+        getWindowVisibleDisplayFrame(r);
+        return r.bottom - r.top;
+    }
+
+    protected int enableKeyEvents() {
+        return (0
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH);
+    }
+
+    protected int disableKeyEvents() {
+        return (0
+                | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH);
+    }
+
+    protected void setPosition(int gravity) {
+        mLayoutParams = (WindowManager.LayoutParams) getLayoutParams();
+        mLayoutParams.gravity = Gravity.TOP | gravity;
+        mWM.updateViewLayout(this, mLayoutParams);
+        invalidate();
+    }
+
+    protected void showTriggerRegion() {
+        setBackgroundResource(R.drawable.trigger_region);
+    }
+
+    protected void hideTriggerRegion() {
+        setBackgroundColor(0);
+    }
+
+    protected void setTopPercentage(float value) {
+        mLayoutParams = (WindowManager.LayoutParams)this.getLayoutParams();
+        mTriggerTop = (int)(mViewHeight * value);
+        mLayoutParams.y = mTriggerTop;
+        mLayoutParams.height = mTriggerBottom;
+        try {
+            mWM.updateViewLayout(this, mLayoutParams);
+        } catch (Exception e) {
+        }
+    }
+
+    protected void setBottomPercentage(float value) {
+        mLayoutParams = (WindowManager.LayoutParams)this.getLayoutParams();
+        mTriggerBottom = (int)(mViewHeight * value);
+        mLayoutParams.height = mTriggerBottom;
+        try {
+            mWM.updateViewLayout(this, mLayoutParams);
+        } catch (Exception e) {
+        }
+    }
+
+    protected void setTriggerWidth(int value) {
+        mLayoutParams = (WindowManager.LayoutParams)this.getLayoutParams();
+        mTriggerWidth = value;
+        mLayoutParams.width = mTriggerWidth;
+        try {
+            mWM.updateViewLayout(this, mLayoutParams);
+        } catch (Exception e) {
+        }
+    }
+
+    protected void expandFromTriggerRegion() {
+        mLayoutParams = (WindowManager.LayoutParams) getLayoutParams();
+        mLayoutParams.y = 0;
+        Rect r = new Rect();
+        getWindowVisibleDisplayFrame(r);
+        mViewHeight = r.bottom - r.top;
+        mLayoutParams.height = mViewHeight;
+        mLayoutParams.width = LayoutParams.MATCH_PARENT;
+        mLayoutParams.flags = enableKeyEvents();
+        mWM.updateViewLayout(this, mLayoutParams);
+    }
+
+    protected void reduceToTriggerRegion() {
+        mLayoutParams = (WindowManager.LayoutParams) getLayoutParams();
+        mLayoutParams.y = mTriggerTop;
+        mLayoutParams.height = mTriggerBottom;
+        mLayoutParams.width = mTriggerWidth;
+        mLayoutParams.flags = disableKeyEvents();
+        mWM.updateViewLayout(this, mLayoutParams);
+    }
+
+    protected boolean isKeyguardEnabled() {
+        KeyguardManager km = (KeyguardManager)mContext.getSystemService(Context.KEYGUARD_SERVICE);
+        return km.inKeyguardRestrictedInputMode();
+    }
+}
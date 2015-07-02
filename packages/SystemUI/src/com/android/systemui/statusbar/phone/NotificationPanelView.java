f9f25242e3dd1e2e4bce754726c394bf3222b076
Roman Birg <roman@cyngn.com>
2015-07-02 11:32:42 -0700

    SystemUI: fix clock jumping when expanding QS from lockscreen
    commit 4f965c5 fixed recalculating clock position from shade_locked but
    it didn't account for other scenarios such as when the QS is expanding,
    causing the clock to jump when expanding QS from lockscreen.
    
    Change-Id: I0e4f473fbc500f7dcdfa015cbedda07761c3cdb2
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2015,7 +2015,9 @@ public class NotificationPanelView extends PanelView implements
 
         // Hide "No notifications" in QS.
         mNotificationStackScroller.updateEmptyShadeView(mShadeEmpty && !mQsExpanded);
-        if (mStatusBarState == StatusBarState.KEYGUARD) {
+        if (mStatusBarState == StatusBarState.KEYGUARD
+                && (!mQsExpanded || mQsExpandImmediate || mIsExpanding
+                && mQsExpandedWhenExpandingStarted)) {
             positionClockAndNotifications();
         }
     }

77585259f81403c639c8680932ebdc7368a5d874
martinusbe <martinusbe@gmail.com>
2015-09-16 14:14:16 +0200

    Revert "Recents: Delay cleanup of remaining tasks when dismissing"
    This reverts commit 988f687e227dd4b64b90e5d900d37f9d3f3c31c2.

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -560,46 +560,45 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
     }
 
     public void dismissAllTasks() {
-        final ArrayList<Task> tasks = new ArrayList<Task>();
-        tasks.addAll(mStack.getTasks());
+        post(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<Task> tasks = new ArrayList<Task>();
+                tasks.addAll(mStack.getTasks());
                 if (!dismissAll() && tasks.size() > 1) {
                     // Ignore the visible foreground task
                     Task foregroundTask = tasks.get(tasks.size() - 1);
                     tasks.remove(foregroundTask);
                 }
 
-        // Remove visible TaskViews
-        long dismissDelay = 0;
-        int childCount = getChildCount();
-        if (childCount > 0) {
-            int delay = mConfig.taskViewRemoveAnimDuration / childCount;
-            for (int i = 0; i < childCount; i++) {
-                TaskView tv = (TaskView) getChildAt(i);
-                tasks.remove(tv.getTask());
-                tv.dismissTask(dismissDelay);
-                dismissDelay += delay;
-            }
-        }
-
-        // Remove any other Tasks
-        for (Task t : tasks) {
-            if (mStack.getTasks().contains(t)) {
-                mStack.removeTask(t);
-            }
-        }
+               // Remove visible TaskViews
+                int childCount = getChildCount();
+                if (!dismissAll() && childCount > 1) childCount--;
+                for (int i = 0; i < childCount; i++) {
+                    TaskView tv = (TaskView) getChildAt(i);
+                    tasks.remove(tv.getTask());
+                    tv.dismissTask();
                 }
 
-        // removeAllUserTask() can take upwards of two seconds to execute so post
-        // a delayed runnable to run this code once we are done animating
-        postDelayed(new Runnable() {
-            @Override
-            public void run() {
+                int size = tasks.size();
+
+                if (size > 0) {
+                    // Remove possible alive Tasks
+                    for (int i = 0; i < size; i++) {
+                        Task t = tasks.get(i);
+                        if (mStack.getTasks().contains(t)) {
+                            mStack.removeTask(t);
+                        }
+                    }
+                }
+
                 // And remove all the excluded or all the other tasks
                 SystemServicesProxy ssp = RecentsTaskLoader.getInstance().getSystemServicesProxy();
                 if (size > 0) {
                     ssp.removeAllUserTask(UserHandle.myUserId());
                 }
-        }, mConfig.taskViewRemoveAnimDuration);
+            }
+        });
     }
 
     @Override
